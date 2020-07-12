0e38614bfee99e21d5c23fffed91288301080a7b
Prashant Kumar <prashant_kumar1993@hotmail.com>
2020-07-12 13:49:14 +0530

    mongodb commit

--- sample-app/util/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-// Internally Uses MySQL2 Under the Hood
-const sequelize = new Sequelize("node-complete", "root", "password", {
-  dialect: "mysql",
-  host: "localhost",
-});
-
-module.exports = sequelize;
